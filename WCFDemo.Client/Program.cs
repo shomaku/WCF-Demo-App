c119055456a4fe8590fd41803e812b6a54d4687c
Sho Maku <s.maku@spro.tech>
2020-06-23 21:12:30 +0900

    add: create the demo application.

---
+++ WCFDemo.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using WCFDemo.Services;
+
+namespace WCFDemo.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            JsonServiceClient client = new JsonServiceClient();
+
+            const string path = @".\test.json";
+            byte[] bytes = File.ReadAllBytes(@".\test.json");
+
+            var result = client.UploadJson(bytes);
+            Console.WriteLine(result);
+
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCFDemo.Host/IJsonService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace WCFDemo.Host
+{
+    [ServiceContract]
+    public interface IJsonService
+    {
+        [OperationContract]
+        string UploadJson(byte[] bytes);
+    }
+}
